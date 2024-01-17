fa56bbf8aed9e42007c3b26e429f99231b7b34f3
firat <erkolfirat@gmail.com>
2024-01-17 09:47:57 +0100

    gui

--- src/main/java/gui/GuiStarter.java
+++ src/main/java/gui/GuiStarter.java
@@ -2,9 +2,16 @@ package gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
 
 import java.util.Objects;
 
@@ -20,6 +27,20 @@ public class GuiStarter extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/gui/MainScene.fxml")));
+
+/*
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+
+        Label greetings = new Label("Query Builder!");
+        greetings.setTextFill(Color.GREEN);
+        greetings.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+
+        root.getChildren().add(greetings);
+*/
+
         stage.setTitle("Query Builder");
         stage.setScene(new Scene(root, 700, 700));
         stage.show();
